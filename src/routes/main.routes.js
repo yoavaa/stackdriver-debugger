f53b8a45673eac44814bbf352634608e155456da
yoavaa <yoav@wix.com>
2020-05-24 14:01:21 +0300

    added some async logic

--- src/routes/main.routes.js
+++ src/routes/main.routes.js
@@ -2,9 +2,17 @@
 import express from 'express';
 const router = express.Router();
 
+const getOjb = (input) => new Promise((resolve, reject) => {
+   setTimeout(() => {
+     resolve({firstName: 'Hello', lastName: 'World', name: input})
+   }, 50);
+});
+
 // Arrow functions
-router.get('/', (req, res) => {
-  res.send({message: 'Hello World!!'});
+router.get('/', async (req, res) => {
+  let input = req.query.name;
+  let {firstName, lastName, name} = await getOjb(input);
+  res.send({message: `${firstName} ${lastName} - ${name}`});
 });
 // Exporting an object as the default import for this module
 export default router;
